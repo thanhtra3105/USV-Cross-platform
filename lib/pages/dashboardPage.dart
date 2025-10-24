55312a9afe98a8b97f40c26a84317d63465eb920
thanhtra3105 <tpa3105@gmail.com>
2025-10-25 04:43:13 +0700

    add retry button

--- lib/pages/dashboardPage.dart
+++ lib/pages/dashboardPage.dart
@@ -1,43 +1,56 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:usv_project/widgets/history_chart.dart';
 import 'package:usv_project/widgets/DashboardCard.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  void reassemble() {
+    super.reassemble();
+  }
 
   Future<Map<String, String>> fetchUSVData() async {
     await Future.delayed(Duration(seconds: 4));
-    // return {
-    //   "Battery": "92",
-    //   "Heading": "10.8",
-    //   "pH": "7.3",
-    //   "DO": "8.3",
-    //   "COD": "7.6",
-    //   "TSS": "6.4",
-    // };
-    throw Exception("Chỉ được một lỗi chung cho tất cả");
+    return {
+      "Battery": "${Random().nextInt(100)}",
+      "Heading": (Random().nextDouble() * 7 + 5).toStringAsFixed(2),
+      "pH": (Random().nextDouble() * 7 + 5).toStringAsFixed(2),
+      "DO": (Random().nextDouble() * 7 + 5).toStringAsFixed(2),
+      "COD": (Random().nextDouble() * 7 + 5).toStringAsFixed(2),
+      "TSS": (Random().nextDouble() * 7 + 5).toStringAsFixed(2),
+    };
+    // throw Exception("Chỉ được một lỗi chung cho tất cả");
   }
 
   Future<double> fetchPH() async {
     await Future.delayed(Duration(seconds: 2));
-    return 7.2;
+    return double.parse((Random().nextDouble() * 7 + 5).toStringAsFixed(2));
   }
 
   Future<double> fetchDO() async {
     await Future.delayed(Duration(seconds: 4));
-    return 10.4;
+    if (Random().nextBool()) throw Exception("fetch DO data failed");
+    return double.parse((Random().nextDouble() * 7 + 5).toStringAsFixed(2));
   }
 
   Future<double> fetchCOD() async {
     await Future.delayed(Duration(seconds: 1));
-    // return 5.1;
-    throw Exception("fetch COD data failed");
+    if (Random().nextBool()) throw Exception("fetch COD data failed");
+    return double.parse((Random().nextDouble() * 7 + 5).toStringAsFixed(2));
   }
 
   Future<double> fetchTSS() async {
     await Future.delayed(Duration(seconds: 3));
-    return 8.4;
+    return double.parse((Random().nextDouble() * 7 + 5).toStringAsFixed(2));
   }
 
   Future<Map<String, double>> fetchAll() async {
@@ -77,75 +90,79 @@ class DashboardPage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
-            FutureBuilder(
-              future: fetchUSVData(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const CircularProgressIndicator(),
-                      SizedBox(height: 20),
-                      Container(child: Text("Loading...")),
-                    ],
-                  );
-                } else if (snapshot.hasError) {
-                  return Text("Error: ${snapshot.error}");
-                } else if (snapshot.hasData) {
-                  final data = snapshot.data!;
-                  return Padding(
-                    padding: EdgeInsets.all(20),
-                    child: GridView.count(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10,
-                      childAspectRatio: 1.0,
+            Container(
+              height: 350,
+              width: 500,
+              child: FutureBuilder(
+                future: fetchUSVData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        buildDashboardCard(
-                          "Battery",
-                          "${data["Battery"]}",
-                          "%",
-                          Icons.battery_6_bar_sharp,
-                        ),
-                        buildDashboardCard(
-                          "Heading",
-                          "${data["Heading"]}",
-                          "km/h",
-                          Icons.speed,
-                        ),
-                        buildDashboardCard(
-                          "pH",
-                          "${data["pH"]}",
-                          "mol/L",
-                          Icons.science,
-                        ),
-                        buildDashboardCard(
-                          "DO",
-                          "${data["DO"]}",
-                          "mg/L",
-                          Icons.opacity,
-                        ),
-                        buildDashboardCard(
-                          "COD",
-                          "${data["COD"]}",
-                          "mg/L",
-                          Icons.waves,
-                        ),
-                        buildDashboardCard(
-                          "TSS",
-                          "4.2",
-                          "mg/L",
-                          Icons.analytics,
-                        ),
+                        const CircularProgressIndicator(),
+                        SizedBox(height: 20),
+                        Container(child: Text("Loading...")),
                       ],
-                    ),
-                  );
-                } else {
-                  return const Text("Không có dữ liệu!");
-                }
-              },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("Error: ${snapshot.error}");
+                  } else if (snapshot.hasData) {
+                    final data = snapshot.data!;
+                    return Padding(
+                      padding: EdgeInsets.all(20),
+                      child: GridView.count(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        childAspectRatio: 1.0,
+                        children: [
+                          buildDashboardCard(
+                            "Battery",
+                            "${data["Battery"]}",
+                            "%",
+                            Icons.battery_6_bar_sharp,
+                          ),
+                          buildDashboardCard(
+                            "Heading",
+                            "${data["Heading"]}",
+                            "km/h",
+                            Icons.speed,
+                          ),
+                          buildDashboardCard(
+                            "pH",
+                            "${data["pH"]}",
+                            "mol/L",
+                            Icons.science,
+                          ),
+                          buildDashboardCard(
+                            "DO",
+                            "${data["DO"]}",
+                            "mg/L",
+                            Icons.opacity,
+                          ),
+                          buildDashboardCard(
+                            "COD",
+                            "${data["COD"]}",
+                            "mg/L",
+                            Icons.waves,
+                          ),
+                          buildDashboardCard(
+                            "TSS",
+                            "${data["TSS"]}",
+                            "mg/L",
+                            Icons.analytics,
+                          ),
+                        ],
+                      ),
+                    );
+                  } else {
+                    return const Text("Không có dữ liệu!");
+                  }
+                },
+              ),
             ),
 
             /* dung future.wait()*/
@@ -165,7 +182,21 @@ class DashboardPage extends StatelessWidget {
                       ],
                     );
                   } else if (snapshot.hasError) {
-                    return Center(child: Text(snapshot.error.toString()));
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(snapshot.error.toString()),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: () => setState(() {
+                              reassemble();
+                            }),
+                            child: Text("Retry"),
+                          ),
+                        ],
+                      ),
+                    );
                   } else if (snapshot.hasData) {
                     final data = snapshot.data!;
                     return Padding(
@@ -210,7 +241,7 @@ class DashboardPage extends StatelessWidget {
                           ),
                           buildDashboardCard(
                             "TSS",
-                            "4.2",
+                            "${data["TSS"]}",
                             "mg/L",
                             Icons.analytics,
                           ),
